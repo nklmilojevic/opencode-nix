b1b462762dacb25c419be2f9c86b97628d6152c3
nklmilojevic <9573356+nklmilojevic@users.noreply.github.com>
2026-01-29 06:17:26 +0000

    chore: update opencode to version 1.1.42

--- package.nix
+++ package.nix
@@ -5,24 +5,24 @@
 }:
 
 let
-  version = "1.1.41";
+  version = "1.1.42";
 
   platformInfo = {
     "x86_64-linux" = {
       platform = "linux-x64";
-      sha256 = "0ccqm2zfhzms7pvafs1nff8993zyqbxy70dcx73j3jm35aj3ahia";
+      sha256 = "15k58hzfbrgs016swnp1h7ibc9jzi5l6msanjins4sfkm20sf3mi";
     };
     "aarch64-linux" = {
       platform = "linux-arm64";
-      sha256 = "0lmg4ywplkl82gk75gf9mix90nzc0crjl91wjiyls2i6aqi4g838";
+      sha256 = "1120mh6ckrjz0hqy7v1c7fhs4mgi2zi2kgl1r9h1y5qi4dzbdj23";
     };
     "x86_64-darwin" = {
       platform = "darwin-x64";
-      sha256 = "0wp2ld8flxi9dwcrdbq6xfgg7bjpnzah0c8x6ijxj1r2aba3g5kb";
+      sha256 = "0ipwspzd7ckg65420mcqracib3k0k5fh863fmq9ql437y7yc8w6i";
     };
     "aarch64-darwin" = {
       platform = "darwin-arm64";
-      sha256 = "15zdpk8r4h9bqfxgx648pw1fza723kgqviq18nrw4dkarda1w1nz";
+      sha256 = "03v3b8cl6yfjafvn5kdw80ia2rxm7i8n1gry1xik3p9mg4xff95n";
     };
   };
 
