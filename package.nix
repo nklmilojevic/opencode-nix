e345c42f7e9a723f1dc09a23b95c281b391b7d58
nklmilojevic <9573356+nklmilojevic@users.noreply.github.com>
2026-04-24 16:23:10 +0000

    chore: update opencode to version 1.14.24

--- package.nix
+++ package.nix
@@ -5,24 +5,24 @@
 }:
 
 let
-  version = "1.14.23";
+  version = "1.14.24";
 
   platformInfo = {
     "x86_64-linux" = {
       platform = "linux-x64";
-      sha256 = "0w3n5mzqjcg5141ix8dhm5wks58nn1niyjqkw45z7smcwmi2cfzk";
+      sha256 = "033xyix06kyvdfa3ypxl3mp9ka4rzkj3xfp4p034xnh5r7m0pwck";
     };
     "aarch64-linux" = {
       platform = "linux-arm64";
-      sha256 = "0bxckcv5gkr9q7m1rsnnxnlqyii9g2wvdvhamh2q1216vvafyzfb";
+      sha256 = "0y7670yrwc7xbxshwpmcvgg7qx6ny9lr0xw5cbafz8zb80f8ms99";
     };
     "x86_64-darwin" = {
       platform = "darwin-x64";
-      sha256 = "0z7j0mqzq2snp4iwjnpjcqmc2hkf5pbv839abn0xafsgwybjr3iw";
+      sha256 = "0wi21qjph5k4mjgk5kacsndykx54hd3bgkgj4s7an0dcd12jgk0p";
     };
     "aarch64-darwin" = {
       platform = "darwin-arm64";
-      sha256 = "1ylg7xbhcz4nswdf01amnn8iahl67krnrb2gjpnr9dn7z8isbsjq";
+      sha256 = "0lwq3dq1l7wz6rzxl23kv91x7jgl75vdm5gxwcgy4bcy8bd6jasw";
     };
   };
 
